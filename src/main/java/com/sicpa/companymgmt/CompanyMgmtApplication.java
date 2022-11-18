7c9bf9b6d528fc92e7f9cc6fa9663bec8093c35b
Kathy Flores <jflores@ec.krugercorp.com>
2022-11-18 16:09:00 -0500

    Initial commit

---
+++ src/main/java/com/sicpa/companymgmt/CompanyMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sicpa.companymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicpa/companymgmt/CompanyMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicpa.companymgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
